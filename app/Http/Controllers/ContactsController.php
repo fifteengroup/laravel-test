49eee1dab5311aa7ddf1517b3527069e7acfac43
nvats <navneet_vats@yahoo.com>
2020-07-25 09:27:39 +0100

    Contacts index page slow loading issue fix
    Eager loading relationship rathan than lazy loading to optimise query

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -13,7 +13,7 @@ class ContactsController extends Controller
 {
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = Contact::with(['company', 'contactRole'])->get();
 
         return view('contacts.index', compact('contacts'));
     }
