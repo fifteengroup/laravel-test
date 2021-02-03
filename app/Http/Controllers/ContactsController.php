f49454c7e3ca6241bf933307648d6f32cd293f2a
Sherry Nadeem <sherryar.nadeem@gmail.com>
2021-02-03 15:51:08 +0000

    make contact_role_id mass assignable

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -9,8 +9,15 @@ use App\Http\Requests\CreateContact;
 use App\Http\Requests\UpdateContact;
 use Illuminate\Http\Request;
 
+/**
+ * Class ContactsController
+ * @package App\Http\Controllers
+ */
 class ContactsController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $contacts = Contact::all();
@@ -18,6 +25,9 @@ class ContactsController extends Controller
         return view('contacts.index', compact('contacts'));
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         $contact = new Contact;
@@ -27,13 +37,22 @@ class ContactsController extends Controller
         return view('contacts.create', compact('contact', 'companies', 'contactRoles'));
     }
 
+    /**
+     * @param CreateContact $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(CreateContact $request)
     {
+//        dd($request->all());
         Contact::create($request->all());
 
         return redirect('contacts')->with('alert', 'Contact created!');
     }
 
+    /**
+     * @param Contact $contact
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function edit(Contact $contact)
     {
         $companies = Company::pluck('name', 'id');
@@ -42,6 +61,11 @@ class ContactsController extends Controller
         return view('contacts.edit', compact('contact', 'companies', 'contactRoles'));
     }
 
+    /**
+     * @param UpdateContact $request
+     * @param Contact $contact
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function update(UpdateContact $request, Contact $contact)
     {
         $contact->update($request->all());
